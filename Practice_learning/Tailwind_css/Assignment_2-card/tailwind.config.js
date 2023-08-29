f4672c8fdad45aaa7f8b689bc1e758118cf68f59
yadnika10 <yadnika10@gmail.com>
2023-08-29 17:09:18 +0530

    first commit

---
+++ Practice_learning/Tailwind_css/Assignment_2-card/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Assignment_2-card/dist/*.html',"./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
