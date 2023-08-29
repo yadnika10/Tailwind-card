962374aa542620aed8fd4f8198d651d668dfb7e6
yadnika10 <122971264+yadnika10@users.noreply.github.com>
2023-08-29 18:53:02 +0530

    Delete Practice_learning/Tailwind_css/Assignment_2-card directory

--- Practice_learning/Tailwind_css/Assignment_2-card/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./Assignment_2-card/dist/*.html',"./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
